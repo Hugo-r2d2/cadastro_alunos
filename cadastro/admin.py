496047255724b2d9eff6f54df868a1228ac0d31e
HugoSantos2131 <ihugocs@gmail.com>
2024-04-11 15:48:48 -0400

    adicionando notas

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,40 +1,74 @@
 from django.contrib import admin
-from cadastro.models import Aluno, Professor, Materia
+from cadastro.models import Aluno, Professor, Materia, Nota
+
+class NotaInline(admin.TabularInline):
+    model = Nota
+    extra = 1
 
 class AlunoAdmin(admin.ModelAdmin):
+    inlines = [NotaInline]
+
     list_display = (
         'nome',
         'idade',
         'ano_nascimento',
-        )
+        'display_professores_materias',  # Método personalizado para exibir as matérias e professores
+    )
     
     search_fields = (
         'matricula_aluno',
         'nome'
-        )
+    )
+    
+    def display_professores_materias(self, obj):
+        materias_professores = []
+        for materia in obj.materias_aluno.all():
+            materias_professores.append(f"// {materia.nome} - {materia.professor_responsavel.nome_professor} // ")
+        return " ".join(materias_professores) 
+    
+    display_professores_materias.short_description = 'Matérias e Professores'  # Define o título da coluna
 
 class ProfessorAdmin(admin.ModelAdmin):
     list_display = (
         'nome_professor',
         'idade_professor',
         'ano_nascimento',
-        'materia_responsavel'
-        )
+    )
     
     search_fields = (
         'matricula_professor',
         'nome_professor',
-        )
+    )
  
 class MateriaAdmin(admin.ModelAdmin):
+    inlines = [NotaInline]
+
     list_display = (
         'nome',
-        )
+        'professor_responsavel',
+    )
     
     search_fields = (
         'nome',
-        )
+    )
+
+class NotaAdmin(admin.ModelAdmin):
+    list_display = (
+        'aluno',
+        'materia',
+        'nota1',
+        'nota2',
+        'nota3',
+        'nota4',
+    )
+
+    search_fields = (
+        'aluno__nome',
+        'materia__nome',
+    )
+
 
 admin.site.register(Aluno, AlunoAdmin)
 admin.site.register(Professor, ProfessorAdmin)
 admin.site.register(Materia, MateriaAdmin)
+admin.site.register(Nota, NotaAdmin)
--- cadastro/models.py
+++ cadastro/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
+        
+# Cadastro de Professores
+class Professor(models.Model): # Funcionando
+    matricula_professor = models.AutoField(primary_key=True)
+    nome_professor = models.CharField(max_length=200, null=True, blank=True)
+    idade_professor = models.IntegerField(null=True, blank=True)
+    ano_nascimento = models.DateField(null=True, blank=True)
+    #materia_responsavel = models.ForeignKey(Materia, on_delete=models.PROTECT, default=1)
+
+    def __str__(self):
+        return self.nome_professor
+    
+    def clean(self):
+        if not self.nome_professor:
+            raise ValidationError("O nome do professor deve ser preenchido")
 
 # Cadastro de Materias com Professores Responsáveis
 class Materia(models.Model):
     nome = models.CharField(max_length=200, null=True, blank=True)
-    #professor_responsavel = models.ForeignKey(Professor, on_delete=models.PROTECT )
+    professor_responsavel = models.ForeignKey(Professor, on_delete=models.PROTECT, default=1 )
 
     def __str__(self):
         return self.nome
@@ -16,21 +31,6 @@ class Materia(models.Model):
         
         #if not self.professor_responsavel:
         #    raise ValidationError("O professor responsável deve ser escolhido. ")
-        
-# Cadastro de Professores
-class Professor(models.Model): # Funcionando
-    matricula_professor = models.AutoField(primary_key=True)
-    nome_professor = models.CharField(max_length=200, null=True, blank=True)
-    idade_professor = models.IntegerField(null=True, blank=True)
-    ano_nascimento = models.DateField(null=True, blank=True)
-    materia_responsavel = models.ForeignKey(Materia, on_delete=models.PROTECT, default=1)
-
-    def __str__(self):
-        return self.nome_professor
-    
-    def clean(self):
-        if not self.nome_professor:
-            raise ValidationError("O nome do professor deve ser preenchido")
 
 # Cadastro de Alunos
 class Aluno(models.Model):
@@ -38,6 +38,7 @@ class Aluno(models.Model):
     nome = models.CharField(max_length=200, null=True, blank=True)
     idade = models.IntegerField(null=True, blank=True)
     ano_nascimento = models.DateField(null=True, blank=True)
+    materias_aluno = models.ManyToManyField(Materia)
 
     def __str__(self):
         return self.nome
@@ -46,3 +47,20 @@ class Aluno(models.Model):
         if not self.nome:
             raise ValidationError("O nome do aluno deve ser preenchido.")
         
+class Nota(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    nota1 = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)], default=0)
+    nota2 = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)], default=0)
+    nota3 = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)], default=0)
+    nota4 = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)], default=0)
+
+    def calcular_media(self):
+        return (self.nota1 + self.nota2 + self.nota3 + self.nota4) / 4.0
+
+    def __str__(self):
+        media = self.calcular_media()
+        return f"Nota de {self.aluno.nome} em {self.materia.nome}: {self.nota1}, {self.nota2}, {self.nota3}, {self.nota4} - Média: {media:.2f}"
+
+    class Meta:
+        unique_together = ['aluno', 'materia']
